c5c3c89e7b274c19654af56c939d576c79854d6b
Loy <blortega3@gmail.com>
2025-05-09 14:29:58 +0800

    feats: - Installed react-icons - Changed some icons to better represent their card category

--- src/app/components/assets.tsx
+++ src/app/components/assets.tsx
@@ -3,10 +3,15 @@ import boyngirl from "@/assets/image/boygirl-removebg.png";
 import schedulesvg from "@/assets/svg/schedule-calendar-svgrepo-com.svg";
 import timesvg from "@/assets/svg/clock-check-svgrepo-com.svg";
 import caresvg from "@/assets/svg/medical-pack-svgrepo-com.svg";
+import checkcirclesvg from "@/assets/svg/check-circle-svg.svg";
+import checksvg from "@/assets/svg/check-svgrepo-com.svg";
+
 export const assets = {
   logoImage,
   boyngirl,
   schedulesvg,
   timesvg,
   caresvg,
+  checkcirclesvg,
+  checksvg,
 };

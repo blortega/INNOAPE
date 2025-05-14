eb21ef32bd022c295fee96fa7bafe3576e654b24
Loy <blortega3@gmail.com>
2025-05-14 10:27:42 +0800

    feat: - Design changes to landing page

--- src/app/components/assets.tsx
+++ src/app/components/assets.tsx
@@ -1,4 +1,6 @@
-import logoImage from "@/assets/image/innodatalogo.png";
+import innodataLogo from "@/assets/image/innodatalogo.png";
+import innoAPE from "@/assets/image/innoAPE.png";
+import innodataIcon from "@/assets/image/innodataicon.png";
 import boyngirl from "@/assets/image/boygirl-removebg.png";
 import schedulesvg from "@/assets/svg/schedule-calendar-svgrepo-com.svg";
 import timesvg from "@/assets/svg/clock-check-svgrepo-com.svg";
@@ -7,7 +9,9 @@ import checkcirclesvg from "@/assets/svg/check-circle-svg.svg";
 import checksvg from "@/assets/svg/check-svgrepo-com.svg";
 
 export const assets = {
-  logoImage,
+  innodataIcon,
+  innoAPE,
+  innodataLogo,
   boyngirl,
   schedulesvg,
   timesvg,
